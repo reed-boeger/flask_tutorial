c9d229206d604f3109fb208b2dfafa3e8ffab045
Reed Boeger <reed@stevensenvironmental.com>
2021-12-18 16:21:07 -0500

    fixed path to index.html for new file structure

--- app/app/views.py
+++ app/app/views.py
@@ -6,8 +6,8 @@ from flask import render_template
 
 @app.route("/")
 def index():
-    return render_template('index.html')
+    return render_template('/public/index.html')
 
-@app.route("/about")
+@app.route("/public/about")
 def about():
     return "<h1 style='color: red'>About!!!!</h1>"
